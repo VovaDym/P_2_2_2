26221926b601b449f37aa8c803d8bc949ab66a89
Vladimir Dymov <77827028+VovaDym@users.noreply.github.com>
2025-02-20 16:10:43 +0300

    carServiceImpl_injected_to_controller

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -1,21 +1,28 @@
 package web.controller;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.*;
 import web.model.Car;
+import web.service.CarService;
 import web.service.CarServiceImpl;
 import java.util.List;
 
 @Controller
 @RequestMapping(value = "/cars")
 public class CarController {
+    private final CarServiceImpl carServiceImpl;
+
+    @Autowired
+    public CarController(CarServiceImpl carServiceImpl) {
+        this.carServiceImpl = carServiceImpl;
+    }
 
     @GetMapping()
     public String printCars(@RequestParam(defaultValue="5") Integer count, ModelMap model) {
-        CarServiceImpl carsServiceImpl = new CarServiceImpl();
-        List<Car> qtyCars = carsServiceImpl.carList(count);
+        List<Car> qtyCars = carServiceImpl.carList(count);
         model.addAttribute("cars", qtyCars);
         return "cars";
     }
